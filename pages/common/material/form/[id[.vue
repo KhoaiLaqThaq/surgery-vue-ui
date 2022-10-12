9774b0d154ea0ddd4e0a017c7ea43a4a64e510f9
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-10-12 09:30:44 +0700

    form chỉnh sửa vật tư

---
+++ pages/common/material/form/[id[.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="mt-3">
+        <MaterialForm :id="materialId" :material="material" />
+    </div>
+</template>
+<script>
+import {ref} from 'vue';
+import { useRoute } from 'vue-router';
+
+import MaterialService from '~~/services/model/material.service';
+import MaterialForm from '~~/components/form/MaterialForm.vue';
+
+export default {
+    components: {
+        MaterialForm
+    },
+    setup() {
+        const route = useRoute();
+        const materialId = ref(route.params && route.params.id);
+        const material = ref({});
+
+        function getMaterialById() {
+            if (materialId.value) {
+                MaterialService.getById(materialId.value)
+                .then((response) => {
+                    let responseData = response.data;
+                    if (responseData) material.value = responseData;
+                })
+                .catch((error) => {
+
+                    console.log("Error: ", error);
+                });
+            }
+        }
+        
+        return {
+            materialId,
+            material,
+
+            getMaterialById
+        }
+    },
+    created() {
+        this.getMaterialById();
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
