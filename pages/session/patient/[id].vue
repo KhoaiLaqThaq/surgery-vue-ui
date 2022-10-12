96739ceb2d7c70d54f1e331ef160ef757c923d98
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-10-12 09:30:11 +0700

    Danh sách phiên khám của bệnh nhân

--- pages/session/patient/[id].vue
+++ pages/session/patient/[id].vue
@@ -1,7 +1,19 @@
 <template>
     <div class="mt-3">
-        <div class="d-flex">
-            <TitleHeader :title="titlePage + patientName" />
+        <div class="d-flex mb-3">
+            <TitleHeader :title="titlePage" />
+            <span class="ps-3 text-lg fw-bold">{{ patient.name }}</span>
+        </div>
+        <div class="row">
+            <SessionList
+                class="col-lg-3 col-md-3 col-xs-12" 
+                :sessionList="sessionList" 
+                @onchangeSessionActive="setSessionActive($event)"
+            />
+            <SessionDetail
+                class="col-lg-9 col-md-9 col-xs-12 bg-white radius-5 box"
+                :session="sessionActive"
+            />
         </div>
     </div>
 </template>
@@ -9,22 +21,42 @@
 import {ref} from 'vue';
 import { useRoute } from 'vue-router';
 
+import TabsWrapper from "~~/components/common/tab/TabsWrapper.vue";
+import TabItem from "~~/components/common/tab/TabItem.vue";
 import TitleHeader from '~~/components/common/TitleHeader.vue';
+import SessionList from "~~/components/session/SessionList.vue";
+import SessionDetail from "~~/components/session/SessionDetail.vue";
+
+import {displayLocalDate_DDMMYYYY} from "~~/constants/format-date.js";
 import SessionService from '~~/services/model/session.service';
+import PatientService from '~~/services/model/patient.service';
 
 export default {
-    components: { TitleHeader },
-    data() {
-        return {
-            titlePage: "Danh sách phiên khám của bệnh nhân: "
-        }
-    },
+    components: { TitleHeader, TabItem, TabsWrapper, SessionList, SessionDetail },
     setup() {
         const route = useRoute();
         const { $showToast } = useNuxtApp();
-        const patientName = ref("");
-        const session = ref([]);
+        const patient = ref({});
+        const sessionList = ref([]);
+        const sessionActive = ref({});
         const patientId = ref(route.params && route.params.id);
+
+        function onLoadPatientByPatientId() {
+            if (patientId.value) {
+                PatientService.getById(patientId.value)
+                .then((response) => {
+                    let responseData = response.data;
+                    if (responseData) {
+                        console.log(responseData);
+                        patient.value = responseData
+                    }
+                })
+                .catch((error) => {
+                    $showToast("Tải thông tin bệnh nhân không thành công", "error", 2000);
+                    console.log("Error: ", error);
+                });
+            }
+        }
 
         function onLoadSessionByPatientId() {
             if (patientId.value) {
@@ -32,7 +64,10 @@ export default {
                 .then((response) => {
                     let responseData = response.data;
                     if (responseData) {
-
+                        sessionList.value = responseData;
+                        if (responseData.length > 0) {
+                            sessionActive.value = responseData[0];
+                        }
                     }
                 })
                 .catch((error) => {
@@ -42,15 +77,37 @@ export default {
             }
         }
 
+        function setSessionActive(e) {
+            if (e) {
+                SessionService.getById(e)
+                .then((response) => {
+                    let responseData = response.data;
+                    if (responseData) {
+                        sessionActive.value  = responseData;
+                    }
+                })
+                .catch((error) => {
+                    console.log("Error: ", error);
+                });
+            }
+        }
+
 
         return {
-            patientName,
+            titlePage: "Danh sách phiên khám của bệnh nhân: ",
+            patient,
+            sessionList,
+            sessionActive,
 
-            onLoadSessionByPatientId
+            displayLocalDate_DDMMYYYY,
+            onLoadSessionByPatientId,
+            onLoadPatientByPatientId,
+            setSessionActive
         }
     },
     created() {
         this.onLoadSessionByPatientId();
+        this.onLoadPatientByPatientId();
     }
 }
 </script>
