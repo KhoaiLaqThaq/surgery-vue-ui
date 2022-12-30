90502eda6af39dd66190288458b11e4f8b7bd8e7
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-12-30 09:55:54 +0700

    Xóa các file không cần thiết

--- plugins/fort-awesome.js
+++
@@ -1,9 +0,0 @@
-import { library, config } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
-config.autoAddCss = false;
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon);
-});
